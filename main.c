f62c834b0eea30b51f5c3093a2d04814e5f04391
ElectricCow <lukecphillips2006@gmail.com>
2022-02-27 12:40:30 +0000

    Added appending, writing, creating and exist check. Probably going to stop now.

--- main.c
+++ main.c
@@ -5,11 +5,7 @@
 
 int main(int argc, const char *argv[])
 {
-    CreateFile(argv[3]);
     const char* fil = ReadFile(argv[1]);
-    WriteFile(argv[3], argv[4]);
-
-    AppendFile(argv[1], argv[2]);
     printf("%s", fil);
     return 0;
 }
